4bccc4d1b3436697e4f2d811404ef3eae62a63ab
Brian Nguyen <nguyen.brian70@gmail.com>
2021-07-01 23:14:53 -0400

    Add fonts, colors, and icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+import ToggleIcon from "./assets/toggleicon.svg";
+import KillIcon from "./assets/killicon.svg";
 
 const client = new W3CWebSocket("wss://www.cryptofacilities.com/ws/v1");
 
 type Order = [price: number, size: number];
 
+function formatNumber(num: number) {
+  return num.toLocaleString();
+}
+
+function formatNumberDecimals(num: number) {
+  return num.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
+
 function App() {
   const [buyData, setBuyData] = useState<Order[]>([]);
   const [sellData, setSellData] = useState<Order[]>([]);
-
   useEffect(() => {
     client.onopen = () => {
       console.log("WebSocket Client Connected");
@@ -29,75 +41,76 @@ function App() {
         console.log(data);
         setBuyData(data.asks.splice(0, 16).reverse());
         setSellData(data.bids.splice(0, 16).reverse());
+        //TODO: processing of totals here
       }
     };
   }, []);
   return (
     <div className="App">
-      <div className="center-container">
-        <div id="orderbook">
-          <div id="orderbook-header">
-            <p style={{ display: "block" }}>Order Book</p>
-            <select id="grouping-dropdown" name="grouping">
-              <option value="0.5">Group 0.5</option>
-              <option value="1">Group 1</option>
-              <option value="2.5">Group 2.5</option>
-            </select>
+      <div id="orderbook">
+        <div id="orderbook-header">
+          <p style={{ display: "block" }}>Order Book</p>
+          <select id="grouping-dropdown" name="grouping">
+            <option value="0.5">Group 0.50</option>
+            <option value="1">Group 1</option>
+            <option value="2.5">Group 2.5</option>
+          </select>
+        </div>
+        <div id="orderbook-data">
+          <div className="orderbook-side">
+            <table>
+              <tbody>
+                <tr>
+                  <th className="orderbook-table-header">TOTAL</th>
+                  <th className="orderbook-table-header">SIZE</th>
+                  <th className="orderbook-table-header">PRICE</th>
+                </tr>
+                {buyData.map((data, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{formatNumber(data[0] * data[1])}</td>
+                      <td>{formatNumber(data[1])}</td>
+                      <td className="price-buy">
+                        {formatNumberDecimals(data[0])}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
-          <div id="orderbook-data">
-            <div className="orderbook-side">
-              <table>
-                <tbody>
-                  <tr>
-                    <th className="orderbook-table-header">TOTAL</th>
-                    <th className="orderbook-table-header">SIZE</th>
-                    <th className="orderbook-table-header">PRICE</th>
-                  </tr>
-                  {buyData.map((data, i) => {
-                    return (
-                      <tr key={i}>
-                        <td>{(data[0] * data[1]).toLocaleString()}</td>
-                        <td>{data[1].toLocaleString()}</td>
-                        <td className="price-buy">
-                          {data[0].toLocaleString()}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-            <div className="orderbook-side">
-              <table>
-                <tbody>
-                  <tr>
-                    <th className="orderbook-table-header">PRICE</th>
-                    <th className="orderbook-table-header">SIZE</th>
-                    <th className="orderbook-table-header">TOTAL</th>
-                  </tr>
-                  {sellData.map((data, i) => {
-                    return (
-                      <tr key={i}>
-                        <td className="price-sell">
-                          {data[0].toLocaleString()}
-                        </td>
-                        <td>{data[1].toLocaleString()}</td>
-                        <td>{(data[0] * data[1]).toLocaleString()}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+          <div className="orderbook-side">
+            <table>
+              <tbody>
+                <tr>
+                  <th className="orderbook-table-header">PRICE</th>
+                  <th className="orderbook-table-header">SIZE</th>
+                  <th className="orderbook-table-header">TOTAL</th>
+                </tr>
+                {sellData.map((data, i) => {
+                  return (
+                    <tr key={i}>
+                      <td className="price-sell">
+                        {formatNumberDecimals(data[0])}
+                      </td>
+                      <td>{formatNumber(data[1])}</td>
+                      <td>{formatNumber(data[0] * data[1])}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
-          <div id="feed-buttons">
-            <button className="feed-btn" id="toggle-btn">
-              Toggle Feed
-            </button>
-            <button className="feed-btn" id="kill-btn">
-              Kill Feed
-            </button>
-          </div>
+        </div>
+        <div id="feed-buttons">
+          <button className="feed-btn" id="toggle-btn">
+            <img src={ToggleIcon} className="btn-icon" />
+            Toggle Feed
+          </button>
+          <button className="feed-btn" id="kill-btn">
+            <img src={KillIcon} className="btn-icon" />
+            Kill Feed
+          </button>
         </div>
       </div>
     </div>
